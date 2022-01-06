7b5a1eac274347a42045cd46f9d464e2fad724e4
CSalazar714 <95145502+CSalazar714@users.noreply.github.com>
2022-01-06 00:14:19 -0600

    Page toggles added

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # Battleship
 simple 2 player battleshipgame
+
+Wireframe pages
+Title Page:
+
+Player One/Two Ship Placement Page:
+https://wireframe.cc/bD1WKF
+
+Player One/Two Game Screen:
+https://wireframe.cc/yBGLVP
+
+Next Player Turn Screen:
+https://wireframe.cc/CfaB5V
+
+Winner Screen:
+https://wireframe.cc/TMKjbQ
+
+
+
+
+Resources:
+Toggle functions:
+https://www.wikihow.com/Toggle-HTML-Display-With-JavaScript#:~:text=Here%27s%20a%20quick%20JavaScript%20trick%20to%20control%20display,want%20to%20toggle%20display%20with%20in%20a%20container.
+
+
--- app.js
+++ app.js
@@ -1,9 +1,67 @@
-const columns = 10
-const rows = 10
+const columns = 10;
+const rows = 10;
+const squareSize = 50;
 
 
 const player1Arr = []
 const player2Arr = []
+let titleContent = document.getElementById("Title");
+let p1Select = document.getElementById("playerOneSelect");
+let p2Select = document.getElementById("playerTwoSelect");
+let p1NextStart = document.getElementById("p1NextPlayerStart")
+let p2NextStart = document.getElementById("p2NextPlayerStart")
+let p1NextPlayer = document.getElementById("nextPlayerScreenP1");
+let p2NextPlayer = document.getElementById("nextPlayerScreenP2");
+let p1GameScreen = document.getElementById("playerOneGameScreen");
+let p2GameScreen = document.getElementById("playerTwoGameScreen");
+let winnerScreen = document.getElementById("endGameScreen");
+//This goes from TITLE to PLAYER ONE NEXT PLAYER SCREEN
+
+function toggleP1Select(){
+    titleContent.classList.toggle("hide");
+    p1NextStart.style.display == "none" ? titleContent.style.display = "none" : p1NextStart.style.display = "block";
+}
+//This goes from PLAYER ONE NEXT PLAYER SCREEN to PLAYER ONE SELECT SCREEN
+function toggleP1StartSelect(){
+    p1NextStart.style.display = "none"
+    p1Select.style.display == "none" ? p1NextStart.style.display = "none" : p1Select.style.display = "block";
+}
+//This command goes from PLAYER ONE SELECT SCREEN to PLAYER TWO NEXT PLAYER SCREEN //
+function toggleP2NextPlayer(){
+    p1Select.style.display = "none"
+    p2NextStart.style.display == "none" ? p1Select.style.display = "none" : p2NextStart.style.display = "block";
+}
+//This command goes from PLAYER TWO NEXT PLAYER SCREEN to PLAYER TWO SELECT SCREEN//
+function toggleP2Select(){
+    p2NextStart.style.display = "none"
+    p2Select.style.display == "none" ? p2NextStart.style.display = "none" : p2Select.style.display = "block";
+}
+//This command goes from PLAYER TWO SELECT SCREEN to PLAYER ONE NEXT PLAYER SCREEN//
+function toggleP1NextStartGame(){
+    p2Select.style.display = "none"
+    p1NextPlayer.style.display == "none" ? p2Select.style.display = "none" : p1NextPlayer.style.display = "block";
+}
+//This command goes from PLAYER ONE NEXT PLAYER SCREEN to PLAYER ONE GAME SCREEN//
+function toggleP1GameScreen(){
+    p1NextPlayer.style.display = "none"
+    p1GameScreen.style.display == "none" ? p1NextPlayer.style.display = "none" : p1GameScreen.style.display = "block";
+}
+//This goes from PLAYER ONE GAME SCREEN to PLAYER TWO NEXT PLAYER SCREEN//
+function toggleOnGoingP2Next(){
+    p1GameScreen.style.display = "none"
+    p2NextPlayer.style.display == "none" ? p1GameScreen.style.display = "none" : p2NextPlayer.style.display = "block";
+}
+//This goes from PLAYER TWO NEXT PLAYER SCREEN to PLAYER TWO GAME SCREEN//
+function toggleP2GameScreen(){
+    p2NextPlayer.style.display = "none"
+    p2GameScreen.style.display == "none" ? p2NextPlayer.style.display = "none" : p2GameScreen.style.display = "block";
+}
+//This goes from PLAYER TWO GAME SCREEN to PLAYER ONE NEXT PLAYER SCREEN
+function toggleOnGoingP1Next(){
+    p2GameScreen.style.display = "none"
+    p1NextPlayer.style.display == "none" ? p2GameScreen.style.display = "none" : p1NextPlayer.style.display = "block";
+}
+
 
 
 for(let c = 0; c< columns; c++) {
@@ -15,5 +73,25 @@ for(let c = 0; c< columns; c++) {
         
     }
 }
+let boardgameGrid = document.getElementById("boardgame")
+
+let square = document.createElement("div")
+boardgameGrid.appendchild(square)
 //console.log (player1Arr);
 //console.log (player2Arr);
+square.id = 'x'+ c + r;
+
+let top = c* squareSize;
+let left = r * squareSize
+
+let board = [
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0]]
