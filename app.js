ecc6a8b241a6849eef8c41f2372cc1f759be99b1
CSalazar714 <95145502+CSalazar714@users.noreply.github.com>
2022-01-10 04:00:08 -0600

    Working on HTML code

--- app.js
+++ app.js
@@ -12,65 +12,92 @@ let p2NextPlayer = document.getElementById("nextPlayerScreenP2");
 let p1GameScreen = document.getElementById("playerOneGameScreen");
 let p2GameScreen = document.getElementById("playerTwoGameScreen");
 let winnerScreen = document.getElementById("endGameScreen");
+//Buttons
+submitBtn.addEventListener("click",toggleP1Select)
+P1NextStart.addEventListener("click",toggleP1StartSelect)
+p1SelectStart.addEventListener("click",toggleP2NextPlayer)
+p2NextStart.addEventListener("click",toggleP2Select)
+p2SelectStart.addEventListener("click",toggleP1NextStartGame)
+p1GameStart.addEventListener("click",toggleP1GameScreen)
+p1GameNextStart.addEventListener("click",toggleOnGoingP2Next)
+p2NextPlayer.addEventListener("click",toggleP2GameScreen)
+p2GameScreen.addEventListener("click",toggleOnGoingP1Next)
+
+let ToggleView = document.querySelector("#Pages");
+ToggleView.addEventListener("click", ToggleFunc, false);
+
+function ToggleFunc(event) {
+	if(event.target.className == 'Pages')
+	{
+		let next = event.target.nextElementSibling;
+
+		if(next.style.display == "none"){
+			next.stlye.display = "block";
+		} else {
+			next.style.display = "none";
+		}
+	}
+}
+
+
 //This goes from TITLE to PLAYER ONE NEXT PLAYER SCREEN
 // titleContent.classList.toggle("show")
+// function toggleP1Select(){ console.log("clicked");
+//     titleContent.classList.toggle("hide");
+//     titleContent.classList.toggle("show")
+//     p1NextStart.classList.toggle("show");
+// }
 
-function toggleP1Select(){ console.log("clicked");
-    titleContent.classList.toggle("hide");
-    titleContent.classList.remove("show")
-    p1NextStart.classList.toggle("show");
-}
-
-//This goes from PLAYER ONE NEXT PLAYER SCREEN to PLAYER ONE SELECT SCREEN
-function toggleP1StartSelect(){
-    p1NextStart.classList.toggle("hide");
-    p1NextStart.classList.remove("show");
-    p1Select.classList.toggle("show");
-}
-//This command goes from PLAYER ONE SELECT SCREEN to PLAYER TWO NEXT PLAYER SCREEN //
-function toggleP2NextPlayer(){
-    p1Select.classList.toggle("hide");
-    p1Select.classList.remove("show")
-    p2NextStart.classList.toggle("show")
-}
-//This command goes from PLAYER TWO NEXT PLAYER SCREEN to PLAYER TWO SELECT SCREEN//
-function toggleP2Select(){
-    p2NextStart.classList.toggle("hide");
-    p2NextStart.classList.remove("show");
-    p2Select.classList.toggle("show")
-}
-//This command goes from PLAYER TWO SELECT SCREEN to PLAYER ONE NEXT PLAYER SCREEN//
-function toggleP1NextStartGame(){
-    p2Select.classList.toggle("hide");
-    p2Select.classList.remove("show");
-    p1NextPlayer.classList.toggle("show")
+// //This goes from PLAYER ONE NEXT PLAYER SCREEN to PLAYER ONE SELECT SCREEN
+// function toggleP1StartSelect(){
+//     p1NextStart.classList.toggle("hide");
+//     p1NextStart.classList.remove("show");
+//     p1Select.classList.toggle("show");
+// }
+// //This command goes from PLAYER ONE SELECT SCREEN to PLAYER TWO NEXT PLAYER SCREEN //
+// function toggleP2NextPlayer(){
+//     p1Select.classList.toggle("hide");
+//     p1Select.classList.remove("show")
+//     p2NextStart.classList.toggle("show")
+// }
+// //This command goes from PLAYER TWO NEXT PLAYER SCREEN to PLAYER TWO SELECT SCREEN//
+// function toggleP2Select(){
+//     p2NextStart.classList.toggle("hide");
+//     p2NextStart.classList.remove("show");
+//     p2Select.classList.toggle("show")
+// }
+// //This command goes from PLAYER TWO SELECT SCREEN to PLAYER ONE NEXT PLAYER SCREEN//
+// function toggleP1NextStartGame(){
+//     p2Select.classList.toggle("hide");
+//     p2Select.classList.remove("show");
+//     p1NextPlayer.classList.toggle("show")
     
-}
-//This command goes from PLAYER ONE NEXT PLAYER SCREEN to PLAYER ONE GAME SCREEN//
-function toggleP1GameScreen(){
-    p1NextPlayer.classList.toggle("hide");
-    p1NextPlayer.classList.remove("show");
-    p1GameScreen.classList.toggle("show")
-}
-//This goes from PLAYER ONE GAME SCREEN to PLAYER TWO NEXT PLAYER SCREEN//
-function toggleOnGoingP2Next(){
-    p1GameScreen.classList.toggle("hide");
-    p1GameScreen.classList.remove("show");
-    p2NextPlayer.classList.toggle("show")
-}
-//This goes from PLAYER TWO NEXT PLAYER SCREEN to PLAYER TWO GAME SCREEN//
-function toggleP2GameScreen(){
-    p2NextPlayer.classList.toggle("hide");
-    p2NextPlayer.classList.remove("show");
-    p2GameScreen.classList.toggle("show")
-}
-//This goes from PLAYER TWO GAME SCREEN to PLAYER ONE NEXT PLAYER SCREEN
-function toggleOnGoingP1Next(){
-    p2GameScreen.classList.toggle("hide");
-    p2GameScreen.classList.remove("show");
-    p1NextPlayer.classList.toggle("show")
+// }
+// //This command goes from PLAYER ONE NEXT PLAYER SCREEN to PLAYER ONE GAME SCREEN//
+// function toggleP1GameScreen(){
+//     p1NextPlayer.classList.toggle("hide");
+//     p1NextPlayer.classList.remove("show");
+//     p1GameScreen.classList.toggle("show")
+// }
+// //This goes from PLAYER ONE GAME SCREEN to PLAYER TWO NEXT PLAYER SCREEN//
+// function toggleOnGoingP2Next(){
+//     p1GameScreen.classList.toggle("hide");
+//     p1GameScreen.classList.remove("show");
+//     p2NextPlayer.classList.toggle("show")
+// }
+// //This goes from PLAYER TWO NEXT PLAYER SCREEN to PLAYER TWO GAME SCREEN//
+// function toggleP2GameScreen(){
+//     p2NextPlayer.classList.toggle("hide");
+//     p2NextPlayer.classList.remove("show");
+//     p2GameScreen.classList.toggle("show")
+// }
+// //This goes from PLAYER TWO GAME SCREEN to PLAYER ONE NEXT PLAYER SCREEN
+// function toggleOnGoingP1Next(){
+//     p2GameScreen.classList.toggle("hide");
+//     p2GameScreen.classList.remove("show");
+//     p1NextPlayer.classList.toggle("show")
     
-}
+// }
 
 
 let gameBoardGrid = document.getElementById("board");
